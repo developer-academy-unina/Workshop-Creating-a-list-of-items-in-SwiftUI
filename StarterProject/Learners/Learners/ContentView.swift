fbe61f152b22b3f5ba830e8dd0361da688b61881
Jan Armbrust <jan.armbrust@googlemail.com>
2026-09-23 14:13:17 +0200

    Add starter project state

---
+++ StarterProject/Learners/Learners/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Learners
+//
+//  Created by Jan Armbrust on 23.09.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StarterProject/Learners/Learners/LearnersApp.swift
@@ -0,0 +1,16 @@
+//
+//  LearnersApp.swift
+//  Learners
+//
+//  Created by Jan Armbrust on 23.09.26.
+//
+
+import SwiftUI
+
+@main struct LearnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
